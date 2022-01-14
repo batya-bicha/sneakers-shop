ad38ed43f51e585d23623c997a0ca90cccc2dffa
batya-b <batya.bicha@gmail.com>
2022-01-14 10:21:42 +0300

    creating the layout and adding basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+
 function App() {
   return (
-    <div>
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
 
+        <div className="d-flex">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }
