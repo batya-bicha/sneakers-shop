07658f2e5d48de469b3854250e734bb65e40ae05
batya-b <batya.bicha@gmail.com>
2022-01-18 13:02:08 +0300

    add scss module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const arr = [
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: "/img/sneakers/image1.png" },
+  { name: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: "/img/sneakers/image2.png" }
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,10 +22,16 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {
+            arr.map(obj => (
+              <Card
+                title={obj.name}
+                price={obj.price}
+                imageUrl={obj.imageUrl}
+                onClick={() => console.log(obj)}
+              />
+            ))
+          }
         </div>
       </div>
     </div>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import styles from './Card.module.scss';
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/image1.png" alt="Sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price}</b>
         </div>
-        <button className="button">
+        <button className={styles.button} onClick={props.onClick}>
           <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
       </div>
